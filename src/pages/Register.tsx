882fd99a38ee3c563aff32df79b6a140065a580e
yingshuming <yingshuming@gmail.com>
2025-01-07 19:16:52 +0800

    注册页开发

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,7 +1,61 @@
-import React from 'react';
+import { UserAddOutlined } from '@ant-design/icons';
+import { Space, Typography, Form, Input, Button } from 'antd';
+import { Link } from 'react-router-dom';
+import styles from './Register.module.scss';
+
+import { LOGIN_PATHNAME } from '../router';
+const { Title } = Typography;
+
+interface RegisterFormValues {
+  username: string;
+  password: string;
+  confirmPassword: string;
+  nickname: string;
+}
 
 function Register() {
-  return <div>Register</div>;
+  function onFinish(values: RegisterFormValues) {
+    console.log(values);
+  }
+
+  return (
+    <div className={styles.container}>
+      <Space>
+        <Title level={2}>
+          <UserAddOutlined />
+        </Title>
+        <Title level={2}>注册</Title>
+      </Space>
+      <div className={styles.form}>
+        <Form
+          labelCol={{ span: 6 }}
+          wrapperCol={{ span: 16 }}
+          onFinish={onFinish}
+        >
+          <Form.Item label='用户名' name='username'>
+            <Input />
+          </Form.Item>
+          <Form.Item label='密码' name='password'>
+            <Input />
+          </Form.Item>
+          <Form.Item label='确认密码' name='confirmPassword'>
+            <Input />
+          </Form.Item>
+          <Form.Item label='昵称' name='nickname'>
+            <Input />
+          </Form.Item>
+          <Form.Item wrapperCol={{ span: 16, offset: 6 }}>
+            <Space>
+              <Button type='primary' htmlType='submit'>
+                注册
+              </Button>
+              <Link to={LOGIN_PATHNAME}>已有账号，去登录</Link>
+            </Space>
+          </Form.Item>
+        </Form>
+      </div>
+    </div>
+  );
 }
 
 export default Register;
