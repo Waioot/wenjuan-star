5c2e105635f8bddffc3ec79d7ca4961172ee1bab
yingshuming <yingshuming@gmail.com>
2025-01-07 09:34:47 +0800

    404 页面开发

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,5 +1,19 @@
-import React from 'react';
+import { Result, Button } from 'antd';
+import { useNavigate } from 'react-router-dom';
+import { MANAGE_LIST_PATHNAME } from '../router';
 
 export default function NotFound() {
-  return <div>NotFound</div>;
+  const navigate = useNavigate();
+  return (
+    <Result
+      status='404'
+      title='404'
+      subTitle='对不起，您访问的页面不存在'
+      extra={
+        <Button type='primary' onClick={() => navigate(MANAGE_LIST_PATHNAME)}>
+          返回首页
+        </Button>
+      }
+    />
+  );
 }
