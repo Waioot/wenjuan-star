457a8cf94290f95ab4927abe75784355970a014b
yingshuming <yingshuming@gmail.com>
2025-01-07 09:49:38 +0800

    问卷管理页面完善侧边栏按钮布局

--- src/layouts/ManageLayout.tsx
+++ src/layouts/ManageLayout.tsx
@@ -1,15 +1,51 @@
-import { Outlet, Link } from 'react-router-dom';
+import { Outlet, Link, useLocation } from 'react-router-dom';
 import styles from './ManageLayout.module.scss';
+import {
+  PlusOutlined,
+  BarsOutlined,
+  StarOutlined,
+  DeleteOutlined,
+} from '@ant-design/icons';
+import { Button, Space, Divider } from 'antd';
 
 function Mainlayout() {
+  const { pathname } = useLocation();
   return (
     <div className={styles.container}>
       <div className={styles.left}>
-        Manage layout left
-        <button>创建问卷</button>
-        <Link to='/manage/list'>我的列表</Link>
-        <Link to='/manage/star'>标星问卷</Link>
-        <Link to='/manage/trash'>回收站</Link>
+        <Space direction='vertical'>
+          <Button type='primary' size='large' icon={<PlusOutlined />}>
+            新建问卷
+          </Button>
+          <Divider style={{ borderTop: 'transparent' }} />
+          <Link to='/manage/list'>
+            <Button
+              type={pathname === '/manage/list' ? 'default' : 'text'}
+              size='large'
+              icon={<BarsOutlined />}
+            >
+              我的问卷
+            </Button>
+          </Link>
+          <Link to='/manage/star'>
+            <Button
+              type={pathname === '/manage/star' ? 'default' : 'text'}
+              size='large'
+              icon={<StarOutlined />}
+            >
+              标星问卷
+            </Button>
+          </Link>
+          <Link to='/manage/trash'>
+            <Button
+              type={pathname === '/manage/trash' ? 'default' : 'text'}
+              size='large'
+              icon={<DeleteOutlined />}
+            >
+              回收站
+            </Button>
+          </Link>
+        </Space>
       </div>
       <div className={styles.right}>
         <Outlet />
