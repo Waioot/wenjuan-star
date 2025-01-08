be5891158c83c2eaefa4cf6042bb308676c9679e
yingshuming <yingshuming@gmail.com>
2025-01-08 19:54:56 +0800

    重构UserInfo组件以管理用户会话，实现注销功能，显示用户信息。利用Redux进行状态管理。

--- src/components/UserInfo.tsx
+++ src/components/UserInfo.tsx
@@ -1,12 +1,41 @@
-import { Link } from 'react-router-dom';
+import { FC } from 'react';
+import { Button, message } from 'antd';
+import { Link, useNavigate } from 'react-router-dom';
+import { UserOutlined } from '@ant-design/icons';
+import { useDispatch } from 'react-redux';
 import { LOGIN_PATHNAME } from '../router';
+import { removeToken } from '../utils/user-token';
+import useGetUserInfo from '../hooks/useGetUserInfo';
+import { logoutReducer } from '../store/userReducer';
 
-const UserInfo = () => {
-  return (
+const UserInfo: FC = () => {
+  const nav = useNavigate();
+  const dispatch = useDispatch();
+
+  const { username, nickname } = useGetUserInfo(); // 从 redux 中获取用户信息
+
+  function logout() {
+    dispatch(logoutReducer()); // 清空了 redux user 数据
+    removeToken(); // 清除 token 的存储
+    message.success('退出成功');
+    nav(LOGIN_PATHNAME);
+  }
+
+  const UserInfo = (
     <>
-      <Link to={LOGIN_PATHNAME}>登录</Link>
+      <span style={{ color: '#e8e8e8' }}>
+        <UserOutlined />
+        {nickname}
+      </span>
+      <Button type='link' onClick={logout}>
+        退出
+      </Button>
     </>
   );
+
+  const Login = <Link to={LOGIN_PATHNAME}>登录</Link>;
+
+  return <div>{username ? UserInfo : Login}</div>;
 };
 
 export default UserInfo;
