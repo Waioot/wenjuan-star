aa91c3c28410e11f0e2ed1599be8212783d2fbb6
yingshuming <yingshuming@gmail.com>
2025-01-08 16:14:26 +0800

    使用useGetUserInfo钩子根据用户登录状态动态设置跳转路径

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -2,12 +2,23 @@ import { Space, Typography } from 'antd';
 import { FormOutlined } from '@ant-design/icons';
 import styles from './Logo.module.scss';
 import { Link } from 'react-router-dom';
-import { HOME_PATHNAME } from '../router';
+import { HOME_PATHNAME, MANAGE_LIST_PATHNAME } from '../router';
+import { useState, useEffect } from 'react';
+import useGetUserInfo from '../hooks/useGetUserInfo';
 const { Title } = Typography;
 function Logo() {
+  const { username } = useGetUserInfo();
+
+  const [pathname, setPathname] = useState(HOME_PATHNAME);
+  useEffect(() => {
+    if (username) {
+      setPathname(MANAGE_LIST_PATHNAME);
+    }
+  }, [username]);
+
   return (
     <div className={styles.container}>
-      <Link to={HOME_PATHNAME}>
+      <Link to={pathname}>
         <Space>
           <Title>
             <FormOutlined />
