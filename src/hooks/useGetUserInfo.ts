bb1eb65215ea950ca82e3a1aac642351b4be7fcf
yingshuming <yingshuming@gmail.com>
2025-01-08 16:02:54 +0800

    useGetUserInfo hook 读取 redux 数据

---
+++ src/hooks/useGetUserInfo.ts
@@ -0,0 +1,9 @@
+import { useSelector } from 'react-redux';
+import { StateType } from '../store';
+
+function useGetUserInfo() {
+  const { username, nickname } = useSelector((state: StateType) => state.user);
+  return { username, nickname };
+}
+
+export default useGetUserInfo;
