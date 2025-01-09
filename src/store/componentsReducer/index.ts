81a7866c40879455986f2b103c2f239a102649ad
yingshuming <yingshuming@gmail.com>
2025-01-09 16:02:43 +0800

    feat: 点击添加组件库中组件至画布，支持根据选中ID动态插入新组件

--- src/store/componentsReducer/index.ts
+++ src/store/componentsReducer/index.ts
@@ -35,8 +35,29 @@ const componentsSlice = createSlice({
     changeSelectedId: (state, action) => {
       state.selectedId = action.payload;
     },
+    // 添加组件
+    addComponent: (state, action) => {
+      const newComponent = {
+        ...action.payload,
+      };
+
+      // 当前组件列表中寻找被选中的 id
+      const index = state.componentList.findIndex(
+        c => c.fe_id === state.selectedId
+      );
+      if (index < 0) {
+        // 如果未选中，则添加到组件列表中
+        state.componentList.push(newComponent);
+      } else {
+        // 如果选中，则插入到 index 后面
+        state.componentList.splice(index + 1, 0, newComponent);
+      }
+      // 更新选中 id
+      state.selectedId = newComponent.fe_id;
+    },
   },
 });
 
-export const { resetComponents, changeSelectedId } = componentsSlice.actions;
+export const { resetComponents, changeSelectedId, addComponent } =
+  componentsSlice.actions;
 export default componentsSlice.reducer;
