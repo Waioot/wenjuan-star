bcd7065e6039841ca6af4f9eacf5f9f0ea2c3380
yingshuming <yingshuming@gmail.com>
2025-01-09 10:50:22 +0800

    重构问卷组件：QuestionTitle 和 QuestionInput 组件，提供标题和输入框功能，支持自定义属性

---
+++ src/components/Question/index.ts
@@ -0,0 +1,6 @@
+import { QuestionTitlePropsType } from './QuestionTitle';
+import { QuestionInputPropsType } from './QuestionInput';
+
+export type ComponentPropsType =
+  | QuestionTitlePropsType
+  | QuestionInputPropsType;
