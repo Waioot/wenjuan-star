d0af4b4ffed04f669d7e172318b6e3c0e8477bba
yingshuming <yingshuming@gmail.com>
2025-01-09 16:52:12 +0800

    feat: 在 ComponentProp 组件中添加 onChange 回调以支持动态修改选中组件的属性

--- src/pages/question/Edit/ComponentProp.tsx
+++ src/pages/question/Edit/ComponentProp.tsx
@@ -1,11 +1,15 @@
 import useGetComponentInfo from '../../../hooks/useGetComponentInfo';
 import { getComponentConfigByType } from '../../../components/Question/index';
+import { ComponentPropsType } from '../../../components/Question';
+import { useDispatch } from 'react-redux';
+import { changeComponentProps } from '../../../store/componentsReducer';
 function NoComponent() {
   return (
     <div style={{ textAlign: 'center', marginTop: '20px' }}>未选中组件</div>
   );
 }
 function ComponentProp() {
+  const dispatch = useDispatch();
   const { selectedComponent } = useGetComponentInfo();
   if (!selectedComponent) return <NoComponent />;
 
@@ -13,8 +17,16 @@ function ComponentProp() {
   const componentConfig = getComponentConfigByType(type);
   if (!componentConfig) return <NoComponent />;
 
+  const changeProps = (newProps: ComponentPropsType) => {
+    if (selectedComponent === null) return;
+    // 获取选中的组件 id
+    const { fe_id } = selectedComponent;
+    // 修改组件属性
+    dispatch(changeComponentProps({ fe_id, newProps }));
+  };
+
   const { PropComponent } = componentConfig;
-  return <PropComponent {...props} />;
+  return <PropComponent {...props} onChange={changeProps} />;
 }
 
 export default ComponentProp;
