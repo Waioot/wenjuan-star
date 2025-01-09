9491d8e0b357c05b5056f46a6cb67544c29d04a6
yingshuming <yingshuming@gmail.com>
2025-01-09 14:27:30 +0800

    重构 EditCavas 组件实现组件的动态渲染。引入getComponent函数，根据组件类型和属性检索和渲染组件

--- src/pages/question/Edit/EditCavas.tsx
+++ src/pages/question/Edit/EditCavas.tsx
@@ -1,13 +1,20 @@
 import styles from './EditCavas.module.scss';
 import useGetComponentInfo from '../../../hooks/useGetComponentInfo';
-
-// 临时展示组件
-import QuestionTitle from '../../../components/Question/QuestionTitle/Component';
-import QuestionInput from '../../../components/Question/QuestionInput/Component';
+import { ComponentInfoType } from '../../../store/componentsReducer';
 import { Spin } from 'antd';
+import { getComponentConfig } from '../../../components/Question';
+
 type EditCavasPropsType = {
   loading: boolean;
 };
+
+function getComponent(c: ComponentInfoType) {
+  const { type, props } = c;
+  const componentConfig = getComponentConfig(type);
+  if (!componentConfig) return null;
+  const { Component } = componentConfig;
+  return <Component {...props} />;
+}
 
 function EditCavas({ loading }: EditCavasPropsType) {
   const { componentList } = useGetComponentInfo();
@@ -20,21 +27,14 @@ function EditCavas({ loading }: EditCavasPropsType) {
     );
   return (
     <div className={styles.cavas}>
-      {componentList.map(c => {
-        const { fe_id, type } = c;
-        const component = componentListMap[type];
-        return <div key={fe_id}>{component}</div>;
+      {componentList.map((c: ComponentInfoType) => {
+        const { fe_id } = c;
+        return (
+          <div className={styles['component-wrapper']} key={fe_id}>
+            <div className={styles.components}>{getComponent(c)}</div>
+          </div>
+        );
       })}
-      <div className={styles['component-wrapper']}>
-        <div className={styles.components}>
-          <QuestionTitle />
-        </div>
-      </div>
-      <div className={styles['component-wrapper']}>
-        <div className={styles.components}>
-          <QuestionInput />
-        </div>
-      </div>
     </div>
   );
 }
