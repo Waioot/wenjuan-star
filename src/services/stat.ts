bf5478573c3ed448f41d208c2136c868d11bcad2
yingshuming <yingshuming@gmail.com>
2025-01-16 22:20:19 +0800

    feat: 添加获取问卷统计和组件统计数据的服务函数

--- src/services/stat.ts
+++ src/services/stat.ts
@@ -1,10 +1,21 @@
 import axios, { ResDataType } from './ajax';
 
+// 获取一个问卷的提交数据
 export async function getQuestionStatService(
   questionId: string,
   option: { page: number; pageSize: number } = { page: 1, pageSize: 10 }
 ): Promise<ResDataType> {
   const url = `/api/stat/${questionId}`;
   const data = (await axios.get(url, { params: option })) as ResDataType;
+  return data;
+}
+
+// 获取一个问卷的选项特定选项（组件）的统计数据
+export async function getComponentStatService(
+  questionId: string,
+  componentId: string
+): Promise<ResDataType> {
+  const url = `/api/stat/${questionId}/${componentId}`;
+  const data = (await axios.get(url)) as ResDataType;
   return data;
 }
