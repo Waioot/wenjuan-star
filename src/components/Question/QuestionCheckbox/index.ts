22a27ee12cb759fc296c6d4ea89a474d287fb9ed
yingshuming <yingshuming@gmail.com>
2025-01-16 22:26:54 +0800

    feat: 引入 StatComponent 到 QuestionCheckbox 和 QuestionRadio 组件，实现统计图表组件的统一管理

--- src/components/Question/QuestionCheckbox/index.ts
+++ src/components/Question/QuestionCheckbox/index.ts
@@ -1,5 +1,6 @@
 import Component from './Component';
 import PropComponent from './PropComponent';
+import StatComponent from './StatComponent';
 import { QuestionCheckboxDefaultProps } from './interface';
 
 export * from './interface';
@@ -11,4 +12,5 @@ export default {
   Component, // 画布显示的组件
   PropComponent, // 修改属性
   defaultProps: QuestionCheckboxDefaultProps,
+  StatComponent, // 统计组件
 };
