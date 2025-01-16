b527624b5b21a89e7c58490255285a94691e25d1
yingshuming <yingshuming@gmail.com>
2025-01-16 11:20:29 +0800

    feat: 集成redux-undo用于组件状态管理

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,4 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
+import undoable, { StateWithHistory, excludeAction } from 'redux-undo';
 import userReducer from './userReducer';
 import componentsReducer from './componentsReducer';
 import { UserStateType } from './userReducer';
@@ -8,14 +9,29 @@ import pageInfoReducer from './pageInfoReducer';
 
 export type StateType = {
   user: UserStateType;
-  components: ComponentsStateType;
+  // components: ComponentsStateType;
+  components: StateWithHistory<ComponentsStateType>; // 使用 undo 功能
   pageInfo: PageInfoStateType;
 };
 
 export default configureStore({
   reducer: {
     user: userReducer,
-    components: componentsReducer,
+
+    // 没有使用 undo 功能
+    // components: componentsReducer,
+
+    // 使用 undo 功能
+    components: undoable(componentsReducer, {
+      limit: 20,
+      filter: excludeAction([
+        'components/resetComponents',
+        'components/changeSelectedId',
+        'components/selectPrevComponent',
+        'components/selectNextComponent',
+      ]),
+    }),
+
     pageInfo: pageInfoReducer,
   },
 });
