f2bf5f7ded9d3ac298f13fa5cd142e9fc0881cb6
yingshuming <yingshuming@gmail.com>
2025-01-16 13:55:59 +0800

    feat: 添加 Stat 组件样式和布局，优化加载状态和未发布状态提示

--- src/pages/question/Stat/index.tsx
+++ src/pages/question/Stat/index.tsx
@@ -1,40 +1,56 @@
 import { Button, Result, Spin } from 'antd';
 import { useNavigate } from 'react-router-dom';
-
-import useLoadQuestionData from '../../../hooks/useLoadQuestionData';
-import useGetPageInfo from '../../../hooks/useGetPageInfo';
 import { useTitle } from 'ahooks';
 
+import StatHeader from './StatHeader';
+import useLoadQuestionData from '../../../hooks/useLoadQuestionData';
+import useGetPageInfo from '../../../hooks/useGetPageInfo';
+import styles from './index.module.scss';
 function Stat() {
   const { loading } = useLoadQuestionData();
   const { title, isPublished } = useGetPageInfo();
   useTitle(`问卷统计 - ${title}`);
   const navigate = useNavigate();
 
-  if (loading) {
-    return (
-      <div style={{ textAlign: 'center', marginTop: '200px' }}>
-        <Spin spinning={loading} size='large'></Spin>
-      </div>
-    );
-  }
+  const LodingElement = (
+    <div style={{ textAlign: 'center', marginTop: '200px' }}>
+      <Spin spinning={loading} size='large'></Spin>
+    </div>
+  );
 
-  if (!isPublished) {
+  function genContent() {
+    if (typeof isPublished === 'boolean' && !isPublished) {
+      return (
+        <div style={{ flex: 1 }}>
+          <Result
+            status='warning'
+            title='该页面未发布'
+            extra={
+              <Button type='primary' onClick={() => navigate(-1)}>
+                返回
+              </Button>
+            }
+          />
+        </div>
+      );
+    }
     return (
-      <div style={{ textAlign: 'center', marginTop: '200px' }}>
-        <Result
-          status='warning'
-          title='该页面未发布'
-          extra={
-            <Button type='primary' onClick={() => navigate(-1)}>
-              返回
-            </Button>
-          }
-        />
-      </div>
+      <>
+        <div className={styles.left}>left</div>
+        <div className={styles.main}>main</div>
+        <div className={styles.right}>right</div>
+      </>
     );
   }
-  return <div>Stat page</div>;
+  return (
+    <div className={styles.container}>
+      <StatHeader />
+      <div className={styles['content-wrapper']}>
+        {loading && LodingElement}
+        {!loading && <div className={styles.content}>{genContent()}</div>}
+      </div>
+    </div>
+  );
 }
 
 export default Stat;
