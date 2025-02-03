43ab895efee7c061fab67872ef4b581769ef7fbb
yingshuming <yingshuming@gmail.com>
2025-02-03 17:22:30 +0800

    chore: 添加Vitest和测试库以改进测试基础设施

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import type { ViteUserConfig } from 'vitest/config';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  css: {
-    /* CSS 预处理器 */
-    preprocessorOptions: {
-      scss: {
-        // 引入 mixin.scss 这样就可以在全局中使用 mixin.scss中预定义的变量了
-        // 给导入的路径最后加上 ; 否则会报错
-        // additionalData: '@import "src/assets/styles/mixin.scss";',
-      },
-    },
-  },
-
   server: {
     open: true, // 是否自动打开浏览器
     port: 8000, // 端口号 b端用 8000 c端用 3000
@@ -28,4 +18,10 @@ export default defineConfig({
       },
     },
   },
-});
+
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/setupTests.ts',
+  },
+} as ViteUserConfig);
